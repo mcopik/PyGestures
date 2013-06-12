aae3ce8759e7d827ed64f66378220c11f4d8aff5
Marcin Copik <mcopik@gmail.com>
2013-06-12 10:27:08 +0200

    Implement conversion to grayscale - luminosity&lightness

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #from cv2 import *
 from CameraCapture import *
-captureImg([128,128])
+captureImg([128,128],"image.bmp")
+#captureImg("image.bmp")
+#loadImg("image.bmp")
 #def save():
 #    cv.SaveImage("image.bmp", img)
 # initialize the camera
