1fe34c127cea4ee4e8c400e59ec2e624c0580814
Marcin Copik <mcopik@gmail.com>
2013-06-12 11:30:16 +0200

    Test feed-forward NN example from PyBrain

--- src/main.py
+++ src/main.py
@@ -1,6 +1,80 @@
-#from cv2 import *
-from CameraCapture import *
-convertImg("image.bmp","image_gray.bmp",ConversionType.AVERAGE)
+import matplotlib
+matplotlib.use('Qt4Agg')
+
+import matplotlib.pylab as pylab
+from cv2 import *
+from CameraCapture import loadImg
+from pybrain.tools.shortcuts import *
+from pybrain.structure import SigmoidLayer
+from pybrain.datasets import ClassificationDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.datasets            import ClassificationDataSet
+from pybrain.utilities           import percentError
+from pybrain.tools.shortcuts     import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure.modules   import SoftmaxLayer
+from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
+from scipy import diag, arange, meshgrid, where
+from numpy.random import multivariate_normal
+img = loadImg("image_gray.bmp")
+means = [(-1,0),(2,4),(3,1)]
+cov = [diag([1,1]), diag([0.5,1.2]), diag([1.5,0.7])]
+alldata = ClassificationDataSet(2, 1, nb_classes=3)
+for n in xrange(400):
+    for klass in range(3):
+        input = multivariate_normal(means[klass],cov[klass])
+        alldata.addSample(input, [klass])
+tstdata, trndata = alldata.splitWithProportion( 0.25 )
+trndata._convertToOneOfMany( )
+tstdata._convertToOneOfMany( )
+print "Number of training patterns: ", len(trndata)
+print "Input and output dimensions: ", trndata.indim, trndata.outdim
+print "First sample (input, target, class):"
+print trndata['input'][0], trndata['target'][0], trndata['class'][0]
+fnn = buildNetwork( trndata.indim, 5, trndata.outdim, outclass=SoftmaxLayer )
+trainer = BackpropTrainer( fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
+ticks = arange(-3.,6.,0.2)
+X, Y = meshgrid(ticks, ticks)
+# need column vectors in dataset, not arrays
+griddata = ClassificationDataSet(2,1, nb_classes=3)
+for i in xrange(X.size):
+    griddata.addSample([X.ravel()[i],Y.ravel()[i]], [0])
+griddata._convertToOneOfMany()  # this is still needed to m
+for i in range(20):
+    trainer.trainEpochs( 1 )
+    trnresult = percentError( trainer.testOnClassData(),
+                              trndata['class'] )
+    tstresult = percentError( trainer.testOnClassData(
+           dataset=tstdata ), tstdata['class'] )
+
+    print "epoch: %4d" % trainer.totalepochs, \
+          "  train error: %5.2f%%" % trnresult, \
+          "  test error: %5.2f%%" % tstresult
+    out = fnn.activateOnDataset(griddata)
+    out = out.argmax(axis=1)  # the highest output activation gives the class
+    out = out.reshape(X.shape)
+    figure(1)
+    ioff()  # interactive graphics off
+    clf()   # clear the plot
+    hold(True) # overplot on
+    for c in [0,1,2]:
+        here, _ = where(tstdata['class']==c)
+        plot(tstdata['input'][here,0],tstdata['input'][here,1],'o')
+    if out.max()!=out.min():  # safety check against flat field
+        contourf(X, Y, out)   # plot the contour
+    ion()   # interactive graphics on
+    draw()  # update the plot
+ioff()
+pylab.show()    
+
+#img = captureImg("image_gray.bmp")
+#DS = ClassificationDataSet(img.width*img.height,class_labels=["FIRST","SECOND"])
+#DS.addSample(img,"FIRST")
+#net = buildNetwork(img.width*img.height,100,2,hiddenclass=SigmoidLayer)
+#trainer = BackPropTrainer(net,ds)
+#trainer.trainUntilConvergence()
+
+#convertImg("image.bmp","image_gray.bmp",ConversionType.AVERAGE)
 #captureImg([128,128],"image.bmp")
 #captureImg("image.bmp")
 #loadImg("image.bmp")
