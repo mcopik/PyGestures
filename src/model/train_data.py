f0a9c3840bcac0e91e524789f27e9dda3836922e
Marcin Copik <mcopik@gmail.com>
2013-06-30 21:53:37 +0200

    Implement class for GUI signals.

---
+++ src/model/train_data.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 29, 2013
+
+@author: mcopik
+'''
+
+class TrainData(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
--- src/view/main_window.py
+++ src/view/main_window.py
@@ -4,7 +4,8 @@ Created on Jun 28, 2013
 @author: mcopik
 '''
 
-from PyQt4.QtGui import QMainWindow,QGridLayout,QWidget
+from PyQt4.QtGui import QMainWindow,QGridLayout,QWidget,QPushButton
+from PyQt4.QtCore import SIGNAL
 
 from video_widget import VideoWidget
 
@@ -12,12 +13,16 @@ class MainWindow(object):
     '''
     Main window in View.
     '''
+    MAIN_WINDOW_TITLE = "PyGestures"
     def __init__(self):
         '''
         Constructor
         '''
         self.mainWindow = QMainWindow();
+        self.mainWindow.setWindowTitle(self.MAIN_WINDOW_TITLE)
+        self.mainWindowSignals = MainWindowSignals(self);
         self._createWidgets();
+        self._connectSignals();
         self.mainWindow.setCentralWidget(self.mainWidget);
         self.mainWindow.show();
         
@@ -26,7 +31,21 @@ class MainWindow(object):
         '''
         self.mainWidget = QWidget(self.mainWindow)
         self.videoWidget = VideoWidget()
+        self.trainNetworkButton = QPushButton(self.mainWindow)
         self.layout = QGridLayout();
         self.layout.addWidget(self.videoWidget,0,0)
+        self.layout.addWidget(self.trainNetworkButton,1,0)
         self.mainWidget.setLayout(self.layout)
         
+    def _connectSignals(self):
+        '''
+        '''
+        self.mainWindow.connect(self.trainNetworkButton,SIGNAL("clicked()"),self.mainWindowSignals.trainNetwork)
+
+class MainWindowSignals():
+    '''
+    '''
+    def __init__(self,main_window):
+        self.main_window = main_window
+    def trainNetwork(self):
+        print "OK"
